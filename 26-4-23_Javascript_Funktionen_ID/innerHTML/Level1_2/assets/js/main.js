a1854f9ef05a366866f14c61817da5e28669cd31
Lis-art <lkesselheim@gmail.com>
2023-04-26 21:56:21 +0200

    weitere Kommentare

--- 26-4-23_Javascript_Funktionen_ID/innerHTML/Level1_2/assets/js/main.js
+++ 26-4-23_Javascript_Funktionen_ID/innerHTML/Level1_2/assets/js/main.js
@@ -5,4 +5,7 @@ console.log(mainHeadline);
 
 mainHeadline.innerHTML = ("<h1>Hello World</h1>" + " ");
 mainHeadline.innerHTML += ("<h2>How are you?</h2>");
+// über .innerHTML wird Element dort hizugefügt wo es angesprochen wird
+
 document.write("<h3>end of the element</h3>");
+// über document.write wird Element ganz unten hizugefügt
